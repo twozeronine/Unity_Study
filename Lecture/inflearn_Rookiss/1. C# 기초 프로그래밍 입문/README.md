197b0e6fe25197400387bfd2e0c0c007eee3bd52
twozeronine <amdusias209@gmail.com>
2021-05-26 18:12:55 +0900

    1. C# 기초프로그래밍 / Reflection과 Attribute

---
+++ Lecture/inflearn_Rookiss/1. C# 기초 프로그래밍 입문/README.md
@@ -0,0 +1,15 @@
+# 1. C# 기초 프로그래밍 입문
+
+복습한 내용만 적었습니다.
+
+## TextRPG2
+
+객체지향 문법을 활용한 텍스트 RPG 게임
+
+## Event
+
+C# 문법중 하나인 Event와 Event를 이용한 디자인패턴인 옵저버 패턴
+
+## Reflection
+
+Reflection과 Attribute
---
+++ Lecture/inflearn_Rookiss/1. C# 기초 프로그래밍 입문/Reflection/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Reflection;
+
+namespace Reflection
+{
+  class Program
+  {
+    class Important : System.Attribute
+    {
+      string message;
+      public Important(string message) => this.message = message;
+    }
+    class Monster
+    {
+      //hp입니다. 아주 중요한 정보입니다.
+      [Important("Very Important")]
+      public int hp = 0;
+      protected int attack = 0;
+      private float speed = 0;
+
+      void Attack() { }
+    }
+    static void Main(string[] args)
+    {
+      // Reflection : X - Ray
+      Monster monster = new Monster();
+      Type type = monster.GetType();
+
+      var fields = type.GetFields(BindingFlags.Public
+                    | BindingFlags.NonPublic
+                    | BindingFlags.Static
+                    | BindingFlags.Instance);
+
+      foreach (FieldInfo field in fields)
+      {
+        string access = "protected";
+        if (field.IsPublic)
+          access = "public";
+        else if (field.IsPrivate)
+          access = "private";
+
+        var attributes = field.GetCustomAttributes();
+
+        Console.WriteLine($"{access} {field.FieldType.Name} {field.Name}");
+      }
+    }
+  }
+}
