df14f43e5553347444ea687eaf10e0a02f5a25bb
twozeronine <amdusias209@gmail.com>
2021-05-26 18:27:37 +0900

    1. C# 기초프로그래밍 / Nullable null값 처리 방법에 대하여

---
+++ Lecture/inflearn_Rookiss/1. C# 기초 프로그래밍 입문/Nullable/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace Nullable
+{
+  class Program
+  {
+    class Monster
+    {
+      public int Id { get; set; }
+    }
+    static void Main(string[] args)
+    {
+      // Nullable -> Null + able
+      int? number = null;
+
+      //number에 있는 값 뽑아오기 
+      if (number != null)
+      {
+        int a = number.Value;
+        Console.WriteLine(a);
+      }
+
+      if (number.HasValue)
+      {
+        int a = number.Value;
+        Console.WriteLine(a);
+      }
+
+      //number에 있는 값 뽑아오기 새로운 문법
+      int b = number ?? 0;
+      Console.WriteLine(b);
+
+      Monster monster = null;
+
+      if (monster != null)
+      {
+        int monsterId = monster.Id;
+      }
+
+      // Nullable -> 형식 ?
+      // null
+      int? id = monster?.Id;
+
+    }
+  }
+}
