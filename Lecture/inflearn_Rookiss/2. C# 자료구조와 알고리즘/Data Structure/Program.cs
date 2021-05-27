eb073fc3a46027f924017d3aa29f6576679a4cb1
twozeronine <amdusias209@gmail.com>
2021-05-27 22:59:02 +0900

    2. C# 자료구조와 알고리즘 / BFS 알고리즘 구현

--- Lecture/inflearn_Rookiss/2. C# 자료구조와 알고리즘/Data Structure/Program.cs
+++ Lecture/inflearn_Rookiss/2. C# 자료구조와 알고리즘/Data Structure/Program.cs
@@ -40,17 +40,54 @@ namespace Data_Structure
       };
       List<int>[] adjUnlink2 = new List<int>[]
       {
-        new List<int>() {1,3 },
-        new List<int>() {0,2,3 },
-        new List<int>() {1 },
-        new List<int>() {0,1 },
-        new List<int>() {5},
-        new List<int>() {4 },
+        new List<int>() { 1,3 },
+        new List<int>() { 0,2,3 },
+        new List<int>() { 1 },
+        new List<int>() { 0,1 },
+        new List<int>() { 5 },
+        new List<int>() { 4 },
       };
 
-      bool[] visited = new bool[6];
+
+
+      public void BFS(int start)
+      {
+        bool[] found = new bool[6];
+        int[] parent = new int[6];
+        int[] distance = new int[6];
+
+        Queue<int> q = new Queue<int>();
+        q.Enqueue(start);
+        found[start] = true;
+        parent[start] = start;
+        distance[start] = 0;
+        while (q.Count > 0)
+        {
+          int now = q.Dequeue();
+          Console.WriteLine(now);
+
+          for (int next = 0; next < 6; next++)
+          {
+            if (adj[now, next] == 0) // 인접하지 않았으면 스킵
+              continue;
+            if (found[next]) // 이미 발견한 애라면 스킵
+              continue;
+            q.Enqueue(next);
+            found[next] = true;
+            parent[next] = now;
+            distance[next] = distance[now] + 1;
+          }
+
+        }
+
+
+      }
+
+      #region DFS
       // 1) 우선 now부터 방문하고.
       // 2) now와 연결된 정점들을 하나씩 확인해서, 아직 미발견(미방문) 상태라면 방문한다.
+
+      bool[] visited = new bool[6];
       public void DFS(int now)
       {
         Console.WriteLine(now);
@@ -58,7 +95,7 @@ namespace Data_Structure
 
         for (int next = 0; next < 6; next++)
         {
-          if (adjUnlink[now, next] == 0) // 연결되어 있지 않으면 스킵.
+          if (adj[now, next] == 0) // 연결되어 있지 않으면 스킵.
             continue;
           if (visited[next]) // 이미 방문했으면 스킵.
             continue;
@@ -87,16 +124,16 @@ namespace Data_Structure
             DFS(now);
       }
     }
+
+    #endregion
     static void Main(string[] args)
     {
       // DFS (Depth First Search 깊이 우선 탐색 )
-
       // BFS (Breadth First Search 너비 우선 탐색 )
-
       Graph graph = new Graph();
       // graph.DFS(0);
       // graph.DFS2(0);
-      graph.SearchAll();
+      graph.BFS(0);
     }
   }
 }
