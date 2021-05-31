98b26cc6b808a1c5f2de29dd23e5d0bef704e780
twozeronine <amdusias209@gmail.com>
2021-05-31 15:26:39 +0900

    PlayerController.cs / 플레이어 이동과 관련 내용

---
+++ Lecture/inflearn_Rookiss/3. 유니티 엔진/Transform/PlayerController.cs
@@ -0,0 +1,31 @@
+public class PlayerController : MonoBehaviour
+{
+  [SerializeField]
+  float _speed = 10.0f;
+  void Start()
+  {
+  }
+
+  //GameObject (Player)
+  //Transform
+  //PlayerController (*)
+  void Update()
+  {
+    // Local -> World
+    // TransformDirection
+
+    // World -> Local
+    // InverseTransformDirection
+
+    //tranform.Translate 플레이어가 바라보는 방향으로 계산해서 이동 
+
+    if (Input.GetKey(KeyCode.W))
+      transform.Translate(Vector3.forward * Time.deltaTime * _speed);
+    if (Input.GetKey(KeyCode.S))
+      transform.Translate(Vector3.back * Time.deltaTime * _speed);
+    if (Input.GetKey(KeyCode.A))
+      transform.Translate(Vector3.left * Time.deltaTime * _speed);
+    if (Input.GetKey(KeyCode.D))
+      transform.Translate(Vector3.right * Time.deltaTime * _speed);
+  }
+}
